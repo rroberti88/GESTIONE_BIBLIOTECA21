3a7f3dab51bd15eb13d5359fca365027617d6013
Felice Iandoli <Felice Iandoli@LAPTOP-4O24TSJR.mshome.net>
2025-12-04 23:52:25 +0100

    commit source packages

---
+++ src/main/java/it/unisa/gestionebiblioteca21/actor/Bibliotecario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.gestionebiblioteca21.actor;
+
+/**
+ *
+ * @author Felice Iandoli
+ */
+public class Bibliotecario {
+    private String nome;
+    private String cognome;
+    private String username;
+    
+public Bibliotecario (String nome, String cognome, String username){
+this.nome = nome;
+this.cognome = cognome;
+this.username = username;
+}
+    
+    
+    
+}
