66a2e2e9177ffe3e991cdcd14aeb20c9c0eabedf
rroberti88 <r.roberti@studenti.unisa.it>
2025-12-08 11:33:27 +0100

    Merge origin/main into main

--- src/main/java/it/unisa/gestionebiblioteca21/controller/HomeController.java
+++ src/main/java/it/unisa/gestionebiblioteca21/controller/HomeController.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package it.unisa.gestionebiblioteca21.controller;
+import it.unisa.gestionebiblioteca21.model.Autenticazione;
+import it.unisa.gestionebiblioteca21.view.HomeView;
 
 /**
  *
  * @author Felice Iandoli
  */
 public class HomeController {
-    
+
+    private Autenticazione model;
+    private HomeView view;
+
+    public HomeController (Autenticazione model, HomeView view){
+    this.model = model;
+    this.view = view;
+}
+
+    public void Login (String username, String password){
+    if (model.login(username, password) == true )
+{
+    view.mostraDashboard(username);
+}
+    else {
+    view.mostraErrore("Credenziali non valide");
+}
+}
+    public void Logout(){
+    view.mostraLogin();
+}
 }
