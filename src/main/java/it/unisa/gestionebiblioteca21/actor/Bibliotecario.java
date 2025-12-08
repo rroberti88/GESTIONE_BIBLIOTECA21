10eaaef57be3b61ef03d3b32593890c4ca0e70ae
rroberti88 <r.roberti@studenti.unisa.it>
2025-12-08 10:51:04 +0100

    Commit dello scheletro delle classi

--- src/main/java/it/unisa/gestionebiblioteca21/actor/Bibliotecario.java
+++ src/main/java/it/unisa/gestionebiblioteca21/actor/Bibliotecario.java
@@ -22,7 +22,37 @@ this.nome = nome;
 this.cognome = cognome;
 this.username = username;
 }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
     
-    
-    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+   
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
 }
